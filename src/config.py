2c0b5a3658400ad59745a2efe02d14d99faf4f3d
Joan Morales <19002775@alumnos.utleon.edu.mx>
2024-10-30 18:45:48 -0600

    primer commit 30/10

---
+++ src/config.py
@@ -0,0 +1,10 @@
+class DevelopementConfig():
+    DEBUG = True
+    MYSQL_HOST = '127.0.0.1'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = ''
+    MYSQL_DB = 'api_utl'
+
+config = {
+    'development': DevelopementConfig
+}
